7469fba1abacd7ac3f94673db00b5a57c3fcddf9
dev-nahianrafi <iamalnahiyan@gmail.com>
2024-03-02 17:11:40 +0600

    banner part end

---
+++ js/tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          basecolour: '#4262FF',
+          textcolor: '#050038',
+        },
+        fontFamily: {
+          'inter': ['"Inter", sans-serif'],
+        },
+        maxWidth: {
+            'container': '1110px',
+            'navbar': '1380px'
+        }
+      }
+    }
+  }
