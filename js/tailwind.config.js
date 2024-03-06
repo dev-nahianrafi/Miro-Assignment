6b3d6112bfd815b3f4360e794076247928738d22
dev-nahianrafi <iamalnahiyan@gmail.com>
2024-03-06 22:19:12 +0600

    built the work part

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -5,6 +5,8 @@ tailwind.config = {
           clifford: '#da373d',
           basecolour: '#4262FF',
           textcolor: '#050038',
+          primary: "#050038",
+          secondary: "#4262FF",
         },
         fontFamily: {
           'inter': ['"Inter", sans-serif'],
@@ -12,7 +14,16 @@ tailwind.config = {
         maxWidth: {
             'container': '1140px',
             'navbar': '1380px'
-        }
+        },
+        screens: {
+          'xs': '350px',
+          'sm': '576px',
+          'md': '768px',
+          'lg': '992px',
+          'xl': '1200px',
+          'xxl': '1400px',
+        },
+        
       }
     }
   }
