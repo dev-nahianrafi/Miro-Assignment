f69c936d0bf25f4886be1f5437f9c49cb141f969
dev-nahianrafi <iamalnahiyan@gmail.com>
2024-03-07 12:07:16 +0600

    complete project

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -2,9 +2,6 @@ tailwind.config = {
     theme: {
       extend: {
         colors: {
-          clifford: '#da373d',
-          basecolour: '#4262FF',
-          textcolor: '#050038',
           primary: "#050038",
           secondary: "#4262FF",
         },
@@ -15,15 +12,15 @@ tailwind.config = {
             'container': '1140px',
             'navbar': '1380px'
         },
-        screens: {
-          'xs': '350px',
-          'sm': '576px',
-          'md': '768px',
-          'lg': '992px',
-          'xl': '1200px',
-          'xxl': '1400px',
-        },
         
-      }
+      },
+      screens: {
+        'xs': '350px',
+        'sm': '576px',
+        'md': '768px',
+        'lg': '992px',
+        'xl': '1200px',
+        'xxl': '1400px',
+      },
     }
   }
